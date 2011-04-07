2b2621db76aa440b5330fd36bf9a47568f609891
Développeur Defitech <dev@defitech.ch>
2011-04-07 12:25:23 +0000

    refactoring et maj du todo

--- src/Main.js
+++ src/Main.js
@@ -29,7 +29,7 @@ Library.Main = {
 
     getJson: function(response) {
         try {
-            var json = eval('(' + response.responseText + ')');
+            var json = Ext.decode(response.responseText);
             if (!json.success) {
                 Ext.Msg.show({
                     title: Library.wording.error_title,
@@ -64,65 +64,72 @@ Library.Main = {
     }
 };
 
+Ext.ns('Ext.ux.menu');
+if (typeof Ext.ux.menu.ListMenu != 'undefined') {
+    Ext.override(Ext.ux.menu.ListMenu, {
+        setSelected : function (value) {
+            value = this.selected = [].concat(value);
 
-Ext.override(Ext.ux.menu.ListMenu, {
-    setSelected : function (value) {
-        value = this.selected = [].concat(value);
+            if (this.loaded) {
+                this.items.each(function(item){
+                    item.setChecked(false, true);
+                    for (var i = 0, len = value.length; i < len; i++) {
+                        if (item.itemId == value[i]) {
+                            item.setChecked(true, true);
+                        }
+                    }
+                }, this);
+            } else {
+                // override du ListMenu pour pouvoir setter une valeur de recherche
+                // alors que le combo n'a pas encore ete charge
+                this.store.load({
+                    scope: this,
+                    callback: function() {
+                        this.setSelected(value);
+                    }
+                });
+            }
+        }
+    });
+}
 
-        if (this.loaded) {
-            this.items.each(function(item){
-                item.setChecked(false, true);
-                for (var i = 0, len = value.length; i < len; i++) {
-                    if (item.itemId == value[i]) {
-                        item.setChecked(true, true);
+Ext.ns('Et.form.Action');
+if (typeof Ext.form.Action.Submit != 'undefined') {
+    Ext.override(Ext.form.Action.Submit, {
+        handleResponse : function(response){
+            if(this.form.errorReader){
+                var rs = this.form.errorReader.read(response);
+                var errors = [];
+                if(rs.records){
+                    for(var i = 0, len = rs.records.length; i < len; i++) {
+                        var r = rs.records[i];
+                        errors[i] = r.data;
                     }
                 }
-            }, this);
-        } else {
-            // override du ListMenu pour pouvoir setter une valeur de recherche
-            // alors que le combo n'a pas encore ete charge
-            this.store.load({
-                scope: this,
-                callback: function() {
-                    this.setSelected(value);
+                if(errors.length < 1){
+                    errors = null;
                 }
-            });
-        }
-    }
-});
-
-Ext.override(Ext.form.Field, {
-    getName : function(){
-        // ajout du check si "dom" existe, pour eviter des erreurs
-        return this.rendered && this.el.dom && this.el.dom.name ? this.el.dom.name : this.name || this.id || '';
-    }
-});
-
-Ext.override(Ext.form.Action.Submit, {
-    handleResponse : function(response){
-        if(this.form.errorReader){
-            var rs = this.form.errorReader.read(response);
-            var errors = [];
-            if(rs.records){
-                for(var i = 0, len = rs.records.length; i < len; i++) {
-                    var r = rs.records[i];
-                    errors[i] = r.data;
-                }
+                return {
+                    success : rs.success,
+                    errors : errors
+                };
             }
-            if(errors.length < 1){
-                errors = null;
+            // ajout d'un try catch pour eviter des erreurs de js si la reponse est mal formee
+            try {
+                return Ext.decode(response.responseText);
+            } catch (e) {
+                var msg = response.responseText + '<br/>Javascript: ' + e.toString();
+                return {success: false, error: msg}
             }
-            return {
-                success : rs.success,
-                errors : errors
-            };
         }
-        // ajout d'un try catch pour eviter des erreurs de js si la reponse est mal formee
-        try {
-            return Ext.decode(response.responseText);
-        } catch (e) {
-            var msg = response.responseText + '<br/>Javascript: ' + e.toString();
-            return {success: false, error: msg}
+    });
+}
+
+if (typeof Ext.form.Field != 'undefined') {
+    Ext.override(Ext.form.Field, {
+        getName : function(){
+            // ajout du check si "dom" existe, pour eviter des erreurs
+            return this.rendered && this.el.dom && this.el.dom.name ? this.el.dom.name : this.name || this.id || '';
         }
-    }
-});
+    });
+}
